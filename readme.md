16bcf57f5a449b3b25bae3feca8b0fe4302b3d3c
Gabr1el132 <gabrielfernandesbiro@gmail.com>
2025-01-22 02:41:10 -0300

    primeiro commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+1.0 Definição de estrutura
---
+++ trabalho.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 500
+
+struct dados
+{
+    char data[10];
+    float tempmedia;
+    float precipi;
+    float umid;
+    float vel_vento;
+};
+
+
+int main(){
+    
+}
